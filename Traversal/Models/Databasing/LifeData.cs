963146e55f452df4ee6471d82f2d4323287cde56
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2021-04-03 17:28:16 +1100

    Wrote Life Provider

---
+++ Traversal/Models/Databasing/LifeData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Models.Databasing
+{
+    public class LifeData : PlayerDataModel
+    {
+        public byte Health;
+        public byte Food;
+        public byte Water;
+        public byte Virus;
+        public byte Stamina;
+        public byte Oxygen;
+        public bool Bleeding;
+        public bool Broken;
+        public byte Temperature;
+    }
+}
--- Traversal/PlayerDataProviders/LifeDataProvider.cs
+++ Traversal/PlayerDataProviders/LifeDataProvider.cs
@@ -1,24 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SDG.Unturned;
+﻿using SDG.Unturned;
 using ShimmyMySherbet.MySQL.EF.Core;
 using Traversal.Models;
+using Traversal.Models.Databasing;
 
 namespace Traversal.PlayerDataProviders
 {
     public class LifeDataProvider : IPlayerDataProvider<PlayerLife>
     {
+        public const string TableName = "PlayerData_Life";
+
         public bool Load(PlayerLife instance, MySQLEntityClient database)
         {
-            return false;
+            LifeData data = database.QuerySingle<LifeData>($"SELECT * FROM `{TableName}` WHERE PlayerID=@0 AND Slot=@1 AND ServerID=@2", instance.channel.GetPlayerID(), instance.channel.GetPlayerSlotID(), Traversal.ServerID);
+
+            if (data == null)
+            {
+                return false;
+            }
+
+            instance.SetValue("_health", data.Health);
+            instance.SetValue("_food", data.Food);
+            instance.SetValue("_water", data.Water);
+            instance.SetValue("_virus", data.Virus);
+            instance.SetValue("_stamina", data.Stamina);
+            instance.SetValue("_oxygen", data.Oxygen);
+            instance.SetValue("_isBleeding", data.Bleeding);
+            instance.SetValue("_isBroken", data.Broken);
+            instance.SetValue("_temperature", (EPlayerTemperature)data.Temperature);
+
+            instance.SetValue("wasLoadCalled", true);
+
+            return true;
         }
 
         public bool Save(PlayerLife instance, MySQLEntityClient database)
         {
-            return false;
+            LifeData life = new LifeData()
+            {
+                PlayerID = instance.channel.GetPlayerID(),
+                ServerID = Traversal.ServerID,
+                Slot = instance.channel.GetPlayerSlotID(),
+                Bleeding = instance.isBleeding,
+                Broken = instance.isBroken,
+                Food = instance.food,
+                Health = instance.health,
+                Oxygen = instance.oxygen,
+                Stamina = instance.stamina,
+                Temperature = (byte)instance.temperature,
+                Virus = instance.virus,
+                Water = instance.water
+            };
+            database.InsertUpdate(life, TableName);
+            return true;
         }
     }
 }
