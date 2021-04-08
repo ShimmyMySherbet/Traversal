a5d00679008a74291bc5c8fa4dbfc4b6cc6d88db
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2021-04-08 15:12:35 +1000

    Added global life data provider

--- Traversal/GlobalDataProviders/GlobalLifeProvider.cs
+++ Traversal/GlobalDataProviders/GlobalLifeProvider.cs
@@ -63,6 +63,48 @@ namespace Traversal.GlobalDataProviders
 
         public bool Save(PlayerLifeData data, MySQLEntityClient database, Scope scope)
         {
+            GlobalLifeData global = database.QuerySingle<GlobalLifeData>($"SELECT * FROM {TableName} WHERE PlayerID=@0 AND Slot=@1", data.PlayerID, data.Slot);
+            if (global == null)
+            {
+                global = new GlobalLifeData()
+                {
+                    PlayerID = data.PlayerID,
+                    Slot = data.Slot
+                };
+            }
+
+
+
+            if (scope["Life.Health"])
+                global.Health = data.Health;
+
+            if (scope["Life.Food"])
+                global.Food = data.Food;
+
+            if (scope["Life.Water"])
+                global.Water = data.Water;
+
+            if (scope["Life.Virus"])
+                global.Virus = data.Virus;
+
+            if (scope["Life.Stamina"])
+                global.Stamina = data.Stamina;
+
+            if (scope["Life.Oxygen"])
+                global.Oxygen = data.Oxygen;
+
+            if (scope["Life.Bleeding"])
+                global.Bleeding = data.Bleeding;
+
+            if (scope["Life.Broken"])
+                global.Broken = data.Broken;
+
+            if (scope["Life.Temperature"])
+                global.Temperature = data.Temperature;
+
+
+
+            return true;
         }
     }
 }
