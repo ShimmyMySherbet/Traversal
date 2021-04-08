44f902e4c71aff899fb67fdca1bfa11926d9c169
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2021-04-08 17:29:02 +1000

    Added global quest data provider

--- Traversal/Providers/GlobalDataProviders/GlobalQuestProvider.cs
+++ Traversal/Providers/GlobalDataProviders/GlobalQuestProvider.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ShimmyMySherbet.MySQL.EF.Core;
+﻿using ShimmyMySherbet.MySQL.EF.Core;
 using Traversal.Models;
+using Traversal.Models.Databasing;
 using Traversal.Models.Databasing.Scoped;
 
 namespace Traversal.Providers.GlobalDataProviders
@@ -17,17 +13,100 @@ namespace Traversal.Providers.GlobalDataProviders
 
         public void CheckSchema(MySQLEntityClient database)
         {
-            throw new NotImplementedException();
+            if (!database.TableExists(TableName))
+            {
+                database.CreateTable<GlobalQuestData>(TableName);
+            }
         }
 
         public bool Load(ref PlayerQuestData data, MySQLEntityClient database, Scope scope)
         {
-            throw new NotImplementedException();
+            GlobalQuestData global = database.QuerySingle<GlobalQuestData>($"SELECT * FROM {TableName} WHERE PlayerID=@0 AND Slot=@1", data.PlayerID, data.Slot);
+            if (global == null)
+            {
+                return false;
+            }
+            global.Load();
+
+            if (scope["Quests.Group"])
+            {
+                data.GroupID = global.GroupID;
+                data.GroupRank = global.GroupRank;
+                data.IsMainGroup = global.IsMainGroup;
+            }
+
+            if (scope["Quests.Flags"])
+            {
+                data.Flags = global.Flags;
+            }
+
+            if (scope["Quests.RadioFrequency"])
+            {
+                data.RadioFrequency = global.RadioFrequency;
+            }
+
+            if (scope["Quests.Marker"])
+            {
+                data.MarkerX = global.MarkerX;
+                data.MarkerY = global.MarkerY;
+                data.MarkerZ = global.MarkerZ;
+            }
+
+            if (scope["Quests.Quests"])
+            {
+                data.ActiveQuests = global.ActiveQuests;
+                data.ActiveQuest = global.ActiveQuest;
+            }
+
+            return true;
         }
 
         public bool Save(PlayerQuestData data, MySQLEntityClient database, Scope scope)
         {
-            throw new NotImplementedException();
+            GlobalQuestData global = database.QuerySingle<GlobalQuestData>($"SELECT * FROM {TableName} WHERE PlayerID=@0 AND Slot=@1", data.PlayerID, data.Slot);
+            if (global == null)
+            {
+                global = new GlobalQuestData()
+                {
+                    PlayerID = data.PlayerID,
+                    Slot = data.Slot
+                };
+            }
+            global.Load();
+
+            if (scope["Quests.Group"])
+            {
+                global.GroupID = data.GroupID;
+                global.GroupRank = data.GroupRank;
+                global.IsMainGroup = data.IsMainGroup;
+            }
+
+            if (scope["Quests.Flags"])
+            {
+                global.Flags = data.Flags;
+            }
+
+            if (scope["Quests.RadioFrequency"])
+            {
+                global.RadioFrequency = data.RadioFrequency;
+            }
+
+            if (scope["Quests.Marker"])
+            {
+                global.MarkerX = data.MarkerX;
+                global.MarkerY = data.MarkerY;
+                global.MarkerZ = data.MarkerZ;
+            }
+
+            if (scope["Quests.Quests"])
+            {
+                global.ActiveQuests = data.ActiveQuests;
+                global.ActiveQuest = data.ActiveQuest;
+            }
+
+            global.Save();
+            database.InsertUpdate(global, TableName);
+            return true;
         }
     }
 }
